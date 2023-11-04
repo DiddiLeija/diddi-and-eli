428423ddad4e4306b8b125100b239a655bd32334
Diego Ramirez <dr01191115@gmail.com>
2023-11-04 09:35:16 -0600

    Add `main.py` and a WIP `src` module

---
+++ main.py
@@ -0,0 +1,13 @@
+"""
+Diddi and Eli: A platformer game with scaling challenges.
+"""
+
+import pyxel
+
+from src import One, Two, Three, Four, Menu
+
+class Main:
+    """
+    Main object for the game, though most of the interface
+    is operated by the src-stored objects.
+    """
---
+++ src/__init__.py
@@ -0,0 +1,9 @@
+"Extra code/tools for the game."
+
+__all__ = ("One", "Two", "Three", "Four")
+
+# TODO: fixme -- these four objects should be Python classes
+One = None
+Two = None
+Three = None
+Four = None
