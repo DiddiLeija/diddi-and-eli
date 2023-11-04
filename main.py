4528f15357e4c2484e56f17d8589fba34211c7ac
Diego Ramirez <dr01191115@gmail.com>
2023-11-04 09:40:13 -0600

    Keep working on `main.py`

--- main.py
+++ main.py
@@ -6,8 +6,16 @@ import pyxel
 
 from src import One, Two, Three, Four, Menu
 
+
 class Main:
     """
     Main object for the game, though most of the interface
     is operated by the src-stored objects.
     """
+
+    def __init__(self):
+        pass
+
+
+if __name__ == "__main__":
+    Main()
