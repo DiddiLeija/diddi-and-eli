00f6bc10665db589d804296e0e894bcccda5fd11
Diego Ramirez <dr01191115@gmail.com>
2023-11-14 16:57:06 -0600

    Prepare variable configs for both Diddi and Eli

--- src/characters.py
+++ src/characters.py
@@ -6,15 +6,74 @@ slimehorns, robots, etc), coins, and NPCs.
 
 import pyxel
 
+# === Tool functions (physics, data, etc)
+# TODO: fixme!
+
 # === Players ===
 
 
 class Player1:
-    "Diddi, Player 1, operated using WASD keys."
+    """
+    Diddi, Player 1, operated using WASD keys.
+    """
+
+    def __init__(self, x=0, y=0):
+        self.x = x
+        self.y = y
+        self.initial_setup()
+    
+    def initial_setup(self):
+        """
+        Main variable configurations, which
+        differentiate from Diddi and Eli.
+        """
+        self.key_up = pyxel.KEY_W
+        self.key_left = pyxel.KEY_A
+        self.key_bullet = pyxel.KEY_S
+        self.key_right = pyxel.KEY_D
+        self.imagebank = [
+            (8, 0),   # Right, normal
+            (16, 0),  # Right, walking (1)
+            (24, 0),  # Right, walikng (2)
+            (32, 0),  # Right, jumping
+            (8, 8),   # Left, normal
+            (16, 8),  # Left, walking (1)
+            (24, 8),  # Left, walikng (2)
+            (32, 8),  # Left, jumping
+        ]
+        self.icon = (0, 16)
 
 
-class Player2:
-    "Eli, Player 2, operated with arrow keys."
+class Player2(Player1):
+    """
+    Eli, Player 2, operated with arrow keys.
+    
+    NOTE: this class is inherited from Diddi
+    (Player1) as it uses most of its structure.
+    However, some variables have changed (see
+    'Player2.initial_setup').
+    """
+
+    def initial_setup(self):
+        """
+        Main variable configurations, which
+        differentiate from Diddi and Eli.
+        """
+        self.key_up = pyxel.KEY_UP
+        self.key_left = pyxel.KEY_LEFT
+        self.key_bullet = pyxel.KEY_DOWN
+        self.key_right = pyxel.KEY_UP
+        self.imagebank = [
+            (8, 16),    # Right, normal
+            (16, 16),   # Right, walking (1)
+            (24, 16),   # Right, walikng (2)
+            (32, 16),   # Right, jumping
+            (8, 24),    # Left, normal
+            (16, 24),   # Left, walking (1)
+            (24, 24),   # Left, walikng (2)
+            (32, 24),   # Left, jumping
+        ]
+        self.icon = (0, 24)
 
 
 # === Mobs ===
