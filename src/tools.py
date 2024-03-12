85a225c6c0a7c9e0094c06ba91124a39f6ec3c2d
Diego Ramirez <dr01191115@gmail.com>
2024-03-12 12:03:55 -0600

    Add a safety check to avoid warped/empty save data

--- src/tools.py
+++ src/tools.py
@@ -4,17 +4,41 @@ import io
 import json
 import pyxel
 
+POSSIBLE_LEVELS = (
+    # a list of allowed level names.
+    # NOTE: apparently "menu" should not be allowed as a saved level.
+    "intro",
+    "one",
+    "two",
+    "three",
+    "four",
+    "preboss",
+    "five",
+    "final"
+)
+
+def check_savedata(data):
+    "Internal function to avoid warped/incorrect save data."
+    if data["level"] not in POSSIBLE_LEVELS:
+        data["level"] = "intro"
+    return data
+
 def draw_text(text, x, y):
+    "Draw a pretty text on the screen."
     pyxel.text(x, y, text, 1)
     pyxel.text(x+1, y, text, 7)
 
 def init_class(obj, popt):
+    "Initialize a class and return the object."
     return obj(popt)
 
 def get_savedata():
+    "Read and return the save data."
     with io.open("savedata.json", "r") as js:
-        return json.loads(js.read())
+        load = json.loads(js.read())
+        return check_savedata(load)
 
 def write_savedata(data):
+    "Write the save data from scratch."
     with io.open("savedata.json", "w") as js:
         js.write(json.dumps(data, sort_keys=True, indent=4))
